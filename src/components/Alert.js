900e81d6e0d3c851794cd2b020c470d4557336b3
rahulcodepython <rd21102004@gmail.com>
2022-05-09 01:29:36 +0530

    .

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Alert(props) {
     return (
-        props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
             <strong>{props.alert.msg}</strong>
         </div>
     );
